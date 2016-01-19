8e07ea92badbbf34ac0325002c0c20772f368873
enelramon@gmail.com <enelramon@gmail.com>
2016-01-18 20:05:07 -0400

    Agregando BLL bien....

---
+++ BLL/Salidas.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace BLL
+{
+    public class Salidas : ClaseMaestra
+    {
+        ConexionDb Conexion = new ConexionDb();
+        public int SalidaId { get; set; }
+        public int CuentaId { get; set; }
+        public string Fecha { get; set; }
+        public double Monto { get; set; }
+        public string Observacion { get; set; }
+
+        public Salidas()
+        {
+            this.SalidaId = 0;
+            this.CuentaId = 0;
+            this.Fecha = "";
+            this.Monto = 0;
+            this.Observacion = "";
+
+        }
+
+        public Salidas(int salidaid, int cuentaid, string fecha, double monto, string observacion)
+        {
+            this.SalidaId = salidaid;
+            this.CuentaId = cuentaid;
+            this.Fecha = fecha;
+            this.Monto = monto;
+            this.Observacion = observacion;
+        }
+
+        public override bool Insertar()
+        {
+            bool retorno = false;
+            try
+            {
+                Conexion.Ejecutar(String.Format("Insert Into Salidas (CuentaId, Fecha, Monto, Observacion) Values ({0},'{1}',{2},'{3}')", this.CuentaId, this.Fecha, this.Monto, this.Observacion));
+                retorno = true;
+            }
+            catch (Exception ex) { throw ex; }
+            return retorno;
+        }
+
+        public override bool Editar()
+        {
+            bool retorno = false;
+            try
+            {
+                Conexion.Ejecutar(String.Format("Update Salidas set CuentaId= {0}, Fecha='{1}', Monto= {2}, Observacion= {3} where SalidaId={4}", this.CuentaId, this.Fecha, this.Monto, this.Observacion, this.SalidaId));
+                retorno = true;
+            }
+            catch (Exception ex) { throw ex; }
+            return retorno;
+        }
+
+        public override bool Eliminar()
+        {
+            bool retorno = false;
+            try
+            {
+                Conexion.Ejecutar(String.Format("Delete From Salidas where SalidaId={0}", this.SalidaId));
+                retorno = true;
+            }
+            catch (Exception ex) { throw ex; }
+            return retorno;
+        }
+
+        public override bool Buscar(int IdBuscado)
+        {
+            DataTable dt = new DataTable();
+
+            dt = Conexion.ObtenerDatos("Select * from Salidas Where SalidaId=" + IdBuscado);
+            if (dt.Rows.Count > 0)
+            {
+                this.SalidaId = (int)dt.Rows[0]["SalidaId"];
+                this.CuentaId = (int)dt.Rows[0]["CuentaId"];
+                this.Fecha = dt.Rows[0]["Fecha"].ToString();
+                this.Monto = (double)dt.Rows[0]["Monto"];
+                this.Observacion = dt.Rows[0]["Observacion"].ToString();
+            }
+            return dt.Rows.Count > 0;
+        }
+
+        public override DataTable Listado(string Campos, string Condicion, string Orden)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BLL/Transferencias.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace BLL
+{
+    public class Transferencias : ClaseMaestra
+    {
+
+        public int TransferenciaId { set; get; }
+        public string Fecha { set; get; }
+        public int CuentaDeOrigenId { set; get; }
+        public int CuentaDeDestinoId { set; get; }
+        public float Monto { set; get; }
+        public string Observacion { set; get; }
+        public int UsuarioId { set; get; }
+        public ConexionDb conexion = new ConexionDb();
+
+        public Transferencias(int transferenciaId, string fecha, int cuentaDeOrigenId, int cuentaDeDestinoId, float monto, string observacion, int usuarioId)
+        {
+            this.TransferenciaId = transferenciaId;
+            this.Fecha = fecha;
+            this.CuentaDeOrigenId = cuentaDeOrigenId;
+            this.CuentaDeDestinoId = cuentaDeDestinoId;
+            this.Monto = monto;
+            this.Observacion = observacion;
+            this.UsuarioId = usuarioId;
+        }
+
+        public Transferencias()
+        {
+        }
+
+        public override bool Insertar()
+        {
+            bool retorno = false;
+            try
+            {
+                conexion.Ejecutar(String.Format("Insert Into Transferencias (Fecha, CuentaDeOrigenId, CuentaDeDestinoId, Monto, Observacion, UsuarioId) Values ('{0}',{1},{2},{3},'{4}',{5})", this.Fecha, this.CuentaDeOrigenId, this.CuentaDeDestinoId, this.Monto, this.Observacion, this.UsuarioId));
+                retorno = true;
+            }
+            catch (Exception ex) { throw ex; }
+            return retorno;
+
+        }
+
+        public override bool Editar()
+        {
+            bool retorno = false;
+            try
+            {
+                conexion.Ejecutar(String.Format("Update Transferencias set Fecha='{0}', CuentaDeOrigenId={1}, CuentaDeDestinoId={2}, Monto={3}, Observacion='{4}', UsuarioId={5} where TransferenciaId={6}", this.Fecha, this.CuentaDeOrigenId, this.CuentaDeDestinoId, this.Monto, this.Observacion, this.UsuarioId, this.TransferenciaId));
+                retorno = true;
+            }
+            catch (Exception ex) { throw ex; }
+            return retorno;
+        }
+
+        public override bool Eliminar()
+        {
+            bool retorno = false;
+            try
+            {
+                conexion.Ejecutar(String.Format("Delete From Transferencias where TransferenciaId={0}", this.TransferenciaId));
+                retorno = true;
+            }
+            catch (Exception ex) { throw ex; }
+            return retorno;
+        }
+
+        public override bool Buscar(int IdBuscado)
+        {
+            DataTable dt = new DataTable();
+
+            dt = conexion.ObtenerDatos("Select * from Transferencias Where TransferenciaId=" + IdBuscado);
+            if (dt.Rows.Count > 0)
+            {
+                this.TransferenciaId = (int)dt.Rows[0]["TransferenciaId"];
+                this.Fecha = dt.Rows[0]["Fecha"].ToString();
+                this.CuentaDeOrigenId = (int)dt.Rows[0]["CuentaDeOrigenId"];
+                this.CuentaDeDestinoId = (int)dt.Rows[0]["CuentaDeDestinoId"];
+                //this.Monto = (float)dt.Rows[0]["Monto"];
+                this.Observacion = dt.Rows[0]["Observacion"].ToString();
+                this.UsuarioId = (int)dt.Rows[0]["UsuarioId"];
+            }
+            return dt.Rows.Count > 0;
+        }
+
+        public override DataTable Listado(string Campos, string Condicion, string Orden)
+        {
+            string ordenar = "";
+            if (!Orden.Equals(""))
+                ordenar = " orden by  " + Orden;
+            return conexion.ObtenerDatos(("Select " + Campos + " from Transferencias where " + Condicion + ordenar));
+        }
+    }
+}
