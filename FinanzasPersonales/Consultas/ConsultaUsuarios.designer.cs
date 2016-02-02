5c0c4255270d7ea58d1874fb65832987d847fdfd
KlainerMorales <laklai_94@hotmail.com>
2016-02-01 20:43:03 -0400

    Agregando consulta y Registro

---
+++ FinanzasPersonales/Consultas/ConsultaUsuarios.designer.cs
@@ -0,0 +1,110 @@
+﻿namespace FinanzasPersonales.Consultas
+{
+    partial class ConsultaUsuarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.BuscarcomboBox = new System.Windows.Forms.ComboBox();
+            this.BuscartextBox = new System.Windows.Forms.TextBox();
+            this.Buscarbutton = new System.Windows.Forms.Button();
+            this.UsuariosdataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.UsuariosdataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Buscar por:";
+            // 
+            // BuscarcomboBox
+            // 
+            this.BuscarcomboBox.FormattingEnabled = true;
+            this.BuscarcomboBox.Items.AddRange(new object[] {
+            "UsuarioId",
+            "NombreUsuario"});
+            this.BuscarcomboBox.Location = new System.Drawing.Point(92, 16);
+            this.BuscarcomboBox.Name = "BuscarcomboBox";
+            this.BuscarcomboBox.Size = new System.Drawing.Size(121, 21);
+            this.BuscarcomboBox.TabIndex = 1;
+            // 
+            // BuscartextBox
+            // 
+            this.BuscartextBox.Location = new System.Drawing.Point(235, 17);
+            this.BuscartextBox.Name = "BuscartextBox";
+            this.BuscartextBox.Size = new System.Drawing.Size(100, 20);
+            this.BuscartextBox.TabIndex = 2;
+            // 
+            // Buscarbutton
+            // 
+            this.Buscarbutton.Location = new System.Drawing.Point(360, 16);
+            this.Buscarbutton.Name = "Buscarbutton";
+            this.Buscarbutton.Size = new System.Drawing.Size(75, 23);
+            this.Buscarbutton.TabIndex = 3;
+            this.Buscarbutton.Text = "Buscar";
+            this.Buscarbutton.UseVisualStyleBackColor = true;
+            this.Buscarbutton.Click += new System.EventHandler(this.Buscarbutton_Click);
+            // 
+            // UsuariosdataGridView
+            // 
+            this.UsuariosdataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.UsuariosdataGridView.Location = new System.Drawing.Point(12, 61);
+            this.UsuariosdataGridView.Name = "UsuariosdataGridView";
+            this.UsuariosdataGridView.Size = new System.Drawing.Size(420, 191);
+            this.UsuariosdataGridView.TabIndex = 4;
+            // 
+            // ConsultaUsuarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 286);
+            this.Controls.Add(this.UsuariosdataGridView);
+            this.Controls.Add(this.Buscarbutton);
+            this.Controls.Add(this.BuscartextBox);
+            this.Controls.Add(this.BuscarcomboBox);
+            this.Controls.Add(this.label1);
+            this.Name = "ConsultaUsuarios";
+            this.Text = "ConsultaUsuarios";
+            ((System.ComponentModel.ISupportInitialize)(this.UsuariosdataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox BuscarcomboBox;
+        private System.Windows.Forms.TextBox BuscartextBox;
+        private System.Windows.Forms.Button Buscarbutton;
+        private System.Windows.Forms.DataGridView UsuariosdataGridView;
+    }
+}
---
+++ FinanzasPersonales/Registros/RegistroUsuarios.designer.cs
@@ -0,0 +1,217 @@
+﻿namespace FinanzasPersonales
+{
+    partial class RegistroUsuarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RegistroUsuarios));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Buscarbutton = new System.Windows.Forms.Button();
+            this.Nuevobutton = new System.Windows.Forms.Button();
+            this.Guardarbutton = new System.Windows.Forms.Button();
+            this.Eliminarbutton = new System.Windows.Forms.Button();
+            this.ActivocheckBox = new System.Windows.Forms.CheckBox();
+            this.NombreUsuariotextBox = new System.Windows.Forms.TextBox();
+            this.ContrasenatextBox = new System.Windows.Forms.TextBox();
+            this.Contrasena1textBox = new System.Windows.Forms.TextBox();
+            this.FotoUsuariopictureBox = new System.Windows.Forms.PictureBox();
+            this.UsuarioIdtextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.FotoUsuariopictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Image = ((System.Drawing.Image)(resources.GetObject("label1.Image")));
+            this.label1.Location = new System.Drawing.Point(13, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "UsuarioId:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre Usuario:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Contraseña:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Confirmar Contraseña:";
+            // 
+            // Buscarbutton
+            // 
+            this.Buscarbutton.Location = new System.Drawing.Point(313, 28);
+            this.Buscarbutton.Name = "Buscarbutton";
+            this.Buscarbutton.Size = new System.Drawing.Size(75, 23);
+            this.Buscarbutton.TabIndex = 5;
+            this.Buscarbutton.Text = "Buscar";
+            this.Buscarbutton.UseVisualStyleBackColor = true;
+            this.Buscarbutton.Click += new System.EventHandler(this.Buscarbutton_Click);
+            // 
+            // Nuevobutton
+            // 
+            this.Nuevobutton.Location = new System.Drawing.Point(119, 222);
+            this.Nuevobutton.Name = "Nuevobutton";
+            this.Nuevobutton.Size = new System.Drawing.Size(75, 23);
+            this.Nuevobutton.TabIndex = 6;
+            this.Nuevobutton.Text = "Nuevo";
+            this.Nuevobutton.UseVisualStyleBackColor = true;
+            this.Nuevobutton.Click += new System.EventHandler(this.Nuevobutton_Click);
+            // 
+            // Guardarbutton
+            // 
+            this.Guardarbutton.Location = new System.Drawing.Point(219, 222);
+            this.Guardarbutton.Name = "Guardarbutton";
+            this.Guardarbutton.Size = new System.Drawing.Size(75, 23);
+            this.Guardarbutton.TabIndex = 7;
+            this.Guardarbutton.Text = "Guardar";
+            this.Guardarbutton.UseVisualStyleBackColor = true;
+            this.Guardarbutton.Click += new System.EventHandler(this.Guardarbutton_Click);
+            // 
+            // Eliminarbutton
+            // 
+            this.Eliminarbutton.Location = new System.Drawing.Point(314, 222);
+            this.Eliminarbutton.Name = "Eliminarbutton";
+            this.Eliminarbutton.Size = new System.Drawing.Size(75, 23);
+            this.Eliminarbutton.TabIndex = 8;
+            this.Eliminarbutton.Text = "Eliminar";
+            this.Eliminarbutton.UseVisualStyleBackColor = true;
+            this.Eliminarbutton.Click += new System.EventHandler(this.Eliminarbutton_Click);
+            // 
+            // ActivocheckBox
+            // 
+            this.ActivocheckBox.AutoSize = true;
+            this.ActivocheckBox.Location = new System.Drawing.Point(138, 175);
+            this.ActivocheckBox.Name = "ActivocheckBox";
+            this.ActivocheckBox.Size = new System.Drawing.Size(56, 17);
+            this.ActivocheckBox.TabIndex = 9;
+            this.ActivocheckBox.Text = "Activo";
+            this.ActivocheckBox.UseVisualStyleBackColor = true;
+            // 
+            // NombreUsuariotextBox
+            // 
+            this.NombreUsuariotextBox.Location = new System.Drawing.Point(146, 62);
+            this.NombreUsuariotextBox.Name = "NombreUsuariotextBox";
+            this.NombreUsuariotextBox.Size = new System.Drawing.Size(162, 20);
+            this.NombreUsuariotextBox.TabIndex = 10;
+            // 
+            // ContrasenatextBox
+            // 
+            this.ContrasenatextBox.Location = new System.Drawing.Point(146, 94);
+            this.ContrasenatextBox.Name = "ContrasenatextBox";
+            this.ContrasenatextBox.Size = new System.Drawing.Size(162, 20);
+            this.ContrasenatextBox.TabIndex = 11;
+            // 
+            // Contrasena1textBox
+            // 
+            this.Contrasena1textBox.Location = new System.Drawing.Point(146, 125);
+            this.Contrasena1textBox.Name = "Contrasena1textBox";
+            this.Contrasena1textBox.Size = new System.Drawing.Size(162, 20);
+            this.Contrasena1textBox.TabIndex = 12;
+            // 
+            // FotoUsuariopictureBox
+            // 
+            this.FotoUsuariopictureBox.Location = new System.Drawing.Point(411, 28);
+            this.FotoUsuariopictureBox.Name = "FotoUsuariopictureBox";
+            this.FotoUsuariopictureBox.Size = new System.Drawing.Size(100, 113);
+            this.FotoUsuariopictureBox.TabIndex = 13;
+            this.FotoUsuariopictureBox.TabStop = false;
+            // 
+            // UsuarioIdtextBox
+            // 
+            this.UsuarioIdtextBox.Location = new System.Drawing.Point(146, 31);
+            this.UsuarioIdtextBox.Name = "UsuarioIdtextBox";
+            this.UsuarioIdtextBox.Size = new System.Drawing.Size(100, 20);
+            this.UsuarioIdtextBox.TabIndex = 14;
+            this.UsuarioIdtextBox.TextChanged += new System.EventHandler(this.UsuarioIdtextBox_TextChanged);
+            // 
+            // RegistroUsuarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 268);
+            this.Controls.Add(this.UsuarioIdtextBox);
+            this.Controls.Add(this.FotoUsuariopictureBox);
+            this.Controls.Add(this.Contrasena1textBox);
+            this.Controls.Add(this.ContrasenatextBox);
+            this.Controls.Add(this.NombreUsuariotextBox);
+            this.Controls.Add(this.ActivocheckBox);
+            this.Controls.Add(this.Eliminarbutton);
+            this.Controls.Add(this.Guardarbutton);
+            this.Controls.Add(this.Nuevobutton);
+            this.Controls.Add(this.Buscarbutton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "RegistroUsuarios";
+            this.Text = "RegistroUsuarios";
+            ((System.ComponentModel.ISupportInitialize)(this.FotoUsuariopictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Buscarbutton;
+        private System.Windows.Forms.Button Nuevobutton;
+        private System.Windows.Forms.Button Guardarbutton;
+        private System.Windows.Forms.Button Eliminarbutton;
+        private System.Windows.Forms.CheckBox ActivocheckBox;
+        private System.Windows.Forms.TextBox NombreUsuariotextBox;
+        private System.Windows.Forms.TextBox ContrasenatextBox;
+        private System.Windows.Forms.TextBox Contrasena1textBox;
+        private System.Windows.Forms.PictureBox FotoUsuariopictureBox;
+        private System.Windows.Forms.TextBox UsuarioIdtextBox;
+    }
+}
