b60585b3971bb3653bf4af90ab0e0758c88764f1
Jorge Enmanuel Castro Mendez <cmendezjorgeenmanuel@gmail.com>
2016-02-22 17:17:05 -0600

    Agregando clase PersonasTelefonos

---
+++ BLL/PersonasTelefonos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DAL;
+
+namespace BLL
+{
+    public class PersonasTelefonos
+    {
+        ConexionDb Conexion = new ConexionDb();
+        public int ID { get; set; }
+        public int PersonaId { get; set; }
+        public int TipoId { get; set; }
+        public string Telefono { get; set; }
+
+        public PersonasTelefonos()
+        {
+            this.ID = 0;
+            this.PersonaId = 0;
+            this.TipoId = 0;
+            this.Telefono = "";
+         }
+    }
+}
